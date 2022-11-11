6a6ff90fad4673c980d449678544652310653f73
Hassan Hafeez <hasancheeta@gmail.com>
2022-11-11 13:04:50 +0500

    rolify ahs been set up and I am creating basic Model Project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    
+
+
+
+
+    protected
+
+    def after_sign_in_path_for(_resource)
+        dashboard_path
+    end
+
+    def after_sign_up_path_for(_resource)
+        dashboard_path
+    end
 end
