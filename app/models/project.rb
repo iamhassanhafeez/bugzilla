b86217351ccbdb8c38ae2e8fbab35b3681370886
Hassan Hafeez <hasancheeta@gmail.com>
2022-11-17 13:51:33 +0500

    Stylised the environment with bootstrap css 5.0.2

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+    enum :status, [ :open, :closed ]
+    validates :title, :description, presence: true
+    has_one_attached :image
 end
--- db/migrate/20221111080322_create_projects.rb
+++ db/migrate/20221111080322_create_projects.rb
@@ -1,7 +1,9 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
-
+      t.string :title
+      t.text :description
+      t.integer :status
       t.timestamps
     end
   end
